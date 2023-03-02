dcd3dd4ecb645dd656a3d4df9f9350d6da48088f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-03-02 20:12:39 +0530

    Update start.py

--- RaiChu/Player/start.py
+++ RaiChu/Player/start.py
@@ -14,27 +14,6 @@ from Process.filters import command
 from datetime import datetime
 from Process.decorators import authorized_users_only
 
-START_TIME = datetime.utcnow()
-START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
-TIME_DURATION_UNITS = (
-    ("week", 60 * 60 * 24 * 7),
-    ("day", 60 ** 2 * 24),
-    ("hour", 60 ** 2),
-    ("min", 60),
-    ("sec", 1),
-)
-
-
-async def _human_time_duration(seconds):
-    if seconds == 0:
-        return "inf"
-    parts = []
-    for unit, div in TIME_DURATION_UNITS:
-        amount, seconds = divmod(int(seconds), div)
-        if amount > 0:
-            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
-    return ", ".join(parts)
-
 
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
